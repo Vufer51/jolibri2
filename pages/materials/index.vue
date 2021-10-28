d094823157b1a6c30c014407d3f59f3ad286ab3e
Vufer <kpot.hopa@gmail.com>
2021-10-28 21:39:16 +0300

    Initial comit

---
+++ pages/materials/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+<h4>Известные рабочие материалы</h4>
+    <b-row align-h="end">
+      <b-col cols="auto">новый</b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "materials",
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
