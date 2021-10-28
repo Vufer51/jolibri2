1fb4ecda8fe03363c811b29b9b109514c380cea3
Vufer <kpot.hopa@gmail.com>
2021-10-28 21:37:49 +0300

    Initial comit

---
+++ store/index.js
@@ -0,0 +1,18 @@
+export const state = () => ({
+  materials: [],
+
+});
+
+export const getters = {
+
+};
+export const mutations = {
+
+};
+
+export const actions = {
+  async nuxtServerInit({ dispatch }) {
+    // await dispatch("user/loadUserInfo");
+  }
+
+};
