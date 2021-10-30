80bb5af03560b287bd5cfa5c399e1b2f2c62e54b
Vufer <kpot.hopa@gmail.com>
2021-10-30 17:56:25 +0300

    materials list

---
+++ components/materialsList.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-list-group>
+    <b-list-group-item v-for="(material, n) in materials" :key="n">
+     <b-row>
+       <b-col class="h5">{{material.title}}</b-col>
+     </b-row>
+      <b-row>
+        <b-col>{{material.description}}</b-col>
+        <b-col cols="1" class="text-center">{{material.counterTitle}}</b-col>
+      </b-row>
+    </b-list-group-item>
+  </b-list-group>
+</template>
+
+<script>
+export default {
+  name: "materialsList",
+  props:['materials'],
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/materials/index.vue
+++ pages/materials/index.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
-<h4>Известные рабочие материалы</h4>
+    <h4>Известные рабочие материалы</h4>
     <b-row align-h="end">
       <b-col cols="auto">новый</b-col>
     </b-row>
+    <new-material/>
+    <materials-list :materials="allMaterials"/>
   </div>
 </template>
 
 <script>
+import { mapGetters} from "vuex";
 export default {
   name: "materials",
   data() {
     return {}
+  },
+  computed:{
+    ...mapGetters(['allMaterials'])
   }
 }
 </script>
